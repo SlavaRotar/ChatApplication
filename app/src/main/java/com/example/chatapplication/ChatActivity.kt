948174386551f49fc5131ae577d926a6b838480c
Yarik <slava_point@hotmail.com>
2023-06-03 08:17:49 +0300

    Version beta without encryption

--- app/src/main/java/com/example/chatapplication/ChatActivity.kt
+++ app/src/main/java/com/example/chatapplication/ChatActivity.kt
@@ -1,13 +1,13 @@
 package com.example.chatapplication
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-
+import javax.crypto.Cipher
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -15,8 +15,8 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.getValue
-import com.google.firebase.ktx.Firebase
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
 
 
 class ChatActivity : AppCompatActivity() {
@@ -27,6 +27,7 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var messageAdapter: MessageAdapter
     private lateinit var messageList: ArrayList<Message>
     private lateinit var mDbRef: DatabaseReference
+
     //private logic
     var receiverRoom: String? = null
     var senderRoom: String? = null
@@ -49,19 +50,19 @@ class ChatActivity : AppCompatActivity() {
         messageBox = findViewById(R.id.messageBox)
         sendButton = findViewById(R.id.sentButton)
         messageList = ArrayList()
-        messageAdapter = MessageAdapter(this,messageList)
+        messageAdapter = MessageAdapter(this, messageList)
 
         chatRecyclerView.layoutManager = LinearLayoutManager(this)
         chatRecyclerView.adapter = messageAdapter
 
         //add data to recyclerView
         mDbRef.child("chats").child(senderRoom!!).child("messages")
-            .addValueEventListener(object: ValueEventListener{
+            .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
 
                     messageList.clear()
 
-                    for(postSnapshot in snapshot.children){
+                    for (postSnapshot in snapshot.children) {
 
                         val message = postSnapshot.getValue(Message::class.java)
                         messageList.add(message!!)
@@ -74,17 +75,17 @@ class ChatActivity : AppCompatActivity() {
                 override fun onCancelled(error: DatabaseError) {
 
 
-
                 }
 
             })
 
 
-        //add message to DB
-        sendButton.setOnClickListener{
-
+        sendButton.setOnClickListener {
             val message = messageBox.text.toString()
-            val messageObject = Message(message,senderUid)
+            val password = "yourSecretPassword12345" // Change this to your own secret password
+            val encryptedMessage = encryptMessage(message, password) // Encrypt the message
+
+            val messageObject = Message(encryptedMessage, senderUid)
 
             mDbRef.child("chats").child(senderRoom!!).child("messages").push()
                 .setValue(messageObject).addOnSuccessListener {
@@ -92,8 +93,24 @@ class ChatActivity : AppCompatActivity() {
                         .setValue(messageObject)
                 }
             messageBox.setText("")
-
-
         }
+    }
+    // Function to encrypt the message using AES-256
+    private fun encryptMessage(message: String, password: String): String {
+        val secretKey: SecretKey = generateAESKey(password)
+        val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+        val encryptedBytes = cipher.doFinal(message.toByteArray())
+        return android.util.Base64.encodeToString(encryptedBytes, android.util.Base64.DEFAULT)
+    }
+    // Function to generate a valid AES key with a length of 256 bits
+    private fun generateAESKey(password: String): SecretKey {
+        val salt = byteArrayOf(0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07)
+        val iterations = 4000
+        val keyLength = 256
+        val secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
+        val keySpec = PBEKeySpec(password.toCharArray(), salt, iterations, keyLength)
+        val secretKey = secretKeyFactory.generateSecret(keySpec)
+        return SecretKeySpec(secretKey.encoded, "AES")
     }
 }
--- app/src/main/java/com/example/chatapplication/MessageAdapter.kt
+++ app/src/main/java/com/example/chatapplication/MessageAdapter.kt
@@ -7,41 +7,44 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
+import javax.crypto.Cipher
+import javax.crypto.SecretKey
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+import javax.crypto.spec.SecretKeySpec
 
-
-class MessageAdapter(val context: Context, val messageList: ArrayList<Message>):
+class MessageAdapter(val context: Context, val messageList: ArrayList<Message>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    val ITEM_RECEIVE = 1;
-    val ITEM_SENT = 2;
+    val ITEM_RECEIVE = 1
+    val ITEM_SENT = 2
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
 
-        if (viewType == 1){
+        if (viewType == 1) {
             // inflate receive
-            val  view: View = LayoutInflater.from(context).inflate(R.layout.receive, parent, false)
+            val view: View =
+                LayoutInflater.from(context).inflate(R.layout.receive, parent, false)
             return ReceiveViewHolder(view)
 
-        }else{
+        } else {
             //inflate sent
-            val  view: View = LayoutInflater.from(context).inflate(R.layout.sent, parent, false)
+            val view: View =
+                LayoutInflater.from(context).inflate(R.layout.sent, parent, false)
             return SentViewHolder(view)
 
         }
-
     }
 
     override fun getItemViewType(position: Int): Int {
 
         val currentMessage = messageList[position]
 
-        if (FirebaseAuth.getInstance().currentUser?.uid.equals(currentMessage.senderId)){
+        if (FirebaseAuth.getInstance().currentUser?.uid.equals(currentMessage.senderId)) {
             return ITEM_SENT
-        }else{
+        } else {
             return ITEM_RECEIVE
         }
-
     }
 
     override fun getItemCount(): Int {
@@ -50,29 +53,62 @@ class MessageAdapter(val context: Context, val messageList: ArrayList<Message>):
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val currentMessage = messageList[position]
-        if (holder.javaClass == SentViewHolder::class.java){
+        if (holder.javaClass == SentViewHolder::class.java) {
             //for sent view holder
-
             val viewHolder = holder as SentViewHolder
-            holder.sentMessage.text = currentMessage.message
-        }else{
+            viewHolder.bind(currentMessage, context)
+        } else {
             //for receive view holder
-
             val viewHolder = holder as ReceiveViewHolder
-            holder.reveiveMessage.text = currentMessage.message
+            viewHolder.bind(currentMessage, context)
         }
     }
 
-    class SentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    companion object {
+        private fun generateAESKey(password: String): SecretKey {
+            val salt = byteArrayOf(0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07)
+            val iterations = 4000
+            val keyLength = 256
+            val secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
+            val keySpec = PBEKeySpec(password.toCharArray(), salt, iterations, keyLength)
+            val secretKey = secretKeyFactory.generateSecret(keySpec)
+            return SecretKeySpec(secretKey.encoded, "AES")
+        }
+    }
+
+    class SentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val sentMessage = itemView.findViewById<TextView>(R.id.txt_sent_message)
 
+        fun bind(message: Message, context: Context) {
+            val password = "yourSecretPassword12345" // Change this to your own secret password
+            val decryptedMessage = decryptMessage(message.message!!, password)
+            sentMessage.text = decryptedMessage
+        }
+
+        private fun decryptMessage(message: String, password: String): String {
+            val secretKey: SecretKey = generateAESKey(password)
+            val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+            cipher.init(Cipher.DECRYPT_MODE, secretKey)
+            val decryptedBytes = cipher.doFinal(android.util.Base64.decode(message, android.util.Base64.DEFAULT))
+            return String(decryptedBytes)
+        }
     }
 
-    class ReceiveViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ReceiveViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val receiveMessage = itemView.findViewById<TextView>(R.id.txt_receive_message)
 
-        val reveiveMessage = itemView.findViewById<TextView>(R.id.txt_receive_message)
+        fun bind(message: Message, context: Context) {
+            val password = "yourSecretPassword12345" // Change this to your own secret password
+            val decryptedMessage = decryptMessage(message.message!!, password)
+            receiveMessage.text = decryptedMessage
+        }
 
+        private fun decryptMessage(message: String, password: String): String {
+            val secretKey: SecretKey = generateAESKey(password)
+            val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+            cipher.init(Cipher.DECRYPT_MODE, secretKey)
+            val decryptedBytes = cipher.doFinal(android.util.Base64.decode(message, android.util.Base64.DEFAULT))
+            return String(decryptedBytes)
+        }
     }
-
-
 }
